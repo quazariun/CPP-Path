d63e823d5e4840df51a6fcbec81996580638c0f7
Eduardo Souza <quazariun@gmail.com>
2019-10-12 16:38:21 -0300

    Att 12/08/2019 16:38

--- spoj/CORMENMG/CORMENMG.cpp
+++ spoj/CORMENMG/CORMENMG.cpp
@@ -4,36 +4,40 @@
 using namespace std;
 
 int main(){
-    int n,t,num,result,final_res,last,indice_ini=0,indice_final;
+    int n,t,num,result,final_res,last,indice_ini=0,indice_final=0;
 
-    vector<int> vect;
+    vector<int> vetor_nums;
     cin >> n;
 
     for(int x=0;x<n;++x){
         cin >> t;
         result = 0;
         final_res = 0;
+        vetor_nums.clear();
 
         for(int h=0;h<t;h++){
             cin >> num;
-            vect.push_back(num);
+            vetor_nums.push_back(num);
         }
 
         indice_ini = 0;
-        indice_final=vect.size();
 
-
-        for(int x=indice_ini;x<vect.size();++x){
-                result += vect[x];
+        for(int x=indice_ini;x<vetor_nums.size()*vetor_nums.size();++x){
 
             if(result == 42){
                 final_res++;
-                indice_ini = x+1;
-                x = indice_ini;
-                result += vect[indice_ini];
+                indice_ini = indice_final+1;
                 result = 0;
             }else if(result > 42){
+                result -= vetor_nums[indice_ini];
                 indice_ini++;
+            }else if(result < 42){
+                result += vetor_nums[indice_final];
+                indice_final++;
+            }
+
+            if(indice_final > vetor_nums.size()){
+                break;
             }
         }
 
